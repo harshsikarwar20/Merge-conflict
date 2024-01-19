a641177488ce16db32539dfd07ad8d20290dafc2
Harsh Sikarwar <123385605+harshsikarwar20@users.noreply.github.com>
2024-01-19 22:34:18 +0530

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,90 @@
+// var info = {
+//     "name" : "Harsh",
+//     "company" : "oodles"
+// }
+
+// console.log(typeof info);
+
+// var arr = [1,2,3,4];
+// console.log(typeof arr);
+
+// Operators...
+
+// var name = "tejasvi";
+// console.log(name);
+
+// var a = 10;
+// var b = 5;
+// console.log(a+b);
+// console.log(a-b);
+
+// var age = 20;
+
+// if(age>18){
+//     console.log("Elegible for voting!");
+// }else{
+//     console.log("Not elegible!");
+// }
+
+// Switch cases :-
+
+// var whether = "winter";
+
+// switch(whether){
+//     case "Summer" : 
+//     console.log("Summer season");
+//     break;
+
+//     case "Winter" : 
+//     console.log("Winter season");
+//     break;
+
+//     case "cloudy" : 
+//     console.log("cloudy season");
+//     break;
+
+//     case "Rainy" : 
+//     console.log("Rainy season");
+//     break;
+
+//     default :
+//     console.log("Season out of the course!!! Fool man");
+// }
+
+// Loops :-
+
+// for(var i = 0; i<10; i++){
+//     console.log(i);
+// }
+
+// var i = 10;
+// while(i>=0){
+//     console.log(i);
+//     i--;
+// }
+// var i = 0;
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i!=10);
+
+
+// Functions...
+// print();
+// function print(){
+//     console.log("Harsh sikarwar");
+// }
+
+// factorial("How are you!!");
+
+// function factorial(info){
+//     var a = "Harsh";
+//     console.log(info);
+// }
+
+
+var a = 10;
+var b = 10;
+
+console.log(a!==b);
---
+++ script.js
@@ -0,0 +1,35 @@
+// Data types:-
+
+// Primitive data type
+// Numbers , String , Boolean , Null , undefined , symbol , BigInt
+
+// var a = 2;
+// console.log(typeof a);
+
+// var b = "Hello";
+// console.log(typeof b);
+
+// var c = true;
+// console.log(typeof c);
+
+// var d = null;
+// console.log(typeof d);
+
+// var e = BigInt(560);
+// console.log(typeof e);
+
+// var f;
+// console.log(typeof f);
+
+
+// Non primitive data type
+
+// Objects
+
+// var info = {
+//     "name" : "Harsh",
+//     "company" : "oodles"
+// }
+
+// console.log(info);
+
